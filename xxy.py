2636b464638d5adfceb9596e8194e217e4e53ed4
oskrivon <oskrivon@mail.ru>
2022-09-22 20:03:54 +0300

    add funding rate from web stream by binance

--- xxy.py
+++ xxy.py
@@ -1,7 +1,10 @@
 import websocket
+import json
 
 def on_message(ws, message):
-    print(message)
+    fundings_json = json.loads(message)
+    funding = [x['r'] for x in fundings_json if x['s'] == 'BTCUSDT']
+    print(funding)
 
 def on_error(ws, error):
     print(error)
@@ -14,7 +17,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     #ws = websocket.WebSocketApp("wss://stream.binance.com:9443/stream?streams=ltcbtc@aggTrade/ethbtc@aggTrade",
-    ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/ltcbtc@aggTrade/ethbtc@aggTrade",
+    ws = websocket.WebSocketApp("wss://fstream.binance.com/ws/!markPrice@arr@1s",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
